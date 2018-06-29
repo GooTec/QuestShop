277b7b817fee4244c6f0d55d9d35fd2c8668dbb6
GooTec <21100037@handong.edu>
2018-06-29 17:11:17 +0900

    MAIN PAGE

--- QuestShop/main/urls.py
+++ QuestShop/main/urls.py
@@ -5,5 +5,10 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('work', views.work, name='work'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about_us, name='aboutus'),
+    path('work', views.work, name='work'),
+    path('people', views.people, name='people'),
 
 ]
--- QuestShop/main/views.py
+++ QuestShop/main/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html', {})
+
+def work(request):
+    return render(request, 'main/work.html', {})
+
+def contact(request):
+    return render(request, 'main/contact.html', {})
+
+def people(request):
+    return render(request, 'main/people.html', {})
+
+def about_us(request):
+    return render(request, 'main/about_us.html', {})
